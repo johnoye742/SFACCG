82e33901cbce0bbab11fd065bb0b706896eaf4dd
John Oye <66364739+johnoye742@users.noreply.github.com>
2021-04-11 02:29:47 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var a = document.getElementsByTagName("l");
+var b = location.href;
+window.onload=()=> {
+if(b.includes("/SFACCG/index.html")) {
+var c = document.getElementById("home");
+c.style.backgroundColor = "green";
+}
+};
