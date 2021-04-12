ff54eb66a859f39b42e32f75314db339755c85d1
John Oye <66364739+johnoye742@users.noreply.github.com>
2021-04-12 11:32:43 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var link = location.href;
 
 window.onload=()=> {
-  if(link == "https://johnoye742.github.io/SFACCG/" || link = "https://johnoye742.github.io/SFACCG" || link = "https://johnoye742.github.io/SFACCG/index.html") {
+  if(link == "https://johnoye742.github.io/SFACCG/" || link == "https://johnoye742.github.io/SFACCG" || link == "https://johnoye742.github.io/SFACCG/index.html") {
   var home = document.getElementById("home");
     home.style.backgroundColor="green";
   }
