b04c0cb7d7899a7fd14c721c895742a4dfcecd18
John Oye <66364739+johnoye742@users.noreply.github.com>
2021-04-25 12:53:10 -0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -9,13 +9,39 @@ window.onload=()=> {
 function show() {
 	var nav = document.getElementById("nav");
 
-if(nav.style.height == "70%") {
+if(nav.style.height == "100%") {
 	nav.style.height = "0%";
 	nav.style.visibility = "hidden";
 	
 } else {
 	nav.style.visibility = "visible";
-	nav.style.height = "70%";
+	nav.style.height = "100%";
 	
 }
+}
+window.addEventListener("scroll", scFun);
+
+function scFun() {
+	var p = window.scrollY;
+	var sc = document.getElementById("scroller");
+		const c = ()=> {
+			
+			window.scrollTo(0, 0);
+		};
+	if(p > 0) {
+		
+		sc.style.opacity = "1";
+	sc.style.bottom = "0";
+			
+		sc.onclick = c;
+	} else {
+		sc.style.bottom = "-2em";
+		sc.style.opacity = "0";
+		
+	
+
+	}
+}
+ function scr() {
+	window.scrollY = 0;
 }
